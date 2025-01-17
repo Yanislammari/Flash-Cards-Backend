03606f4065c18ed59d389ed1bb0cfbd508fa19d4
yanislammari <yanis.lammari937@gmail.com>
2025-01-17 21:26:34 +0100

    add card mapper

---
+++ src/shared/mappers/card.mapper.ts
@@ -0,0 +1,19 @@
+import CardSchema from "../../infrastructure/schemas/card.schema";
+import Card from "../../domain/entities/card.entity";
+
+const CardMapper = {
+  toDomain(cardSchema: CardSchema): Card {
+    return {
+      id: cardSchema.id,
+      category: cardSchema.category,
+      question: cardSchema.question,
+      answer: cardSchema.answer,
+    };
+  },
+
+  toDomainList(cardSchemas: CardSchema[]): Card[] {
+    return cardSchemas.map(this.toDomain);
+  },
+};
+
+export default CardMapper;
