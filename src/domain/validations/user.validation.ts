e529020a843fe5d66e01bf211a6398170e074fcd
yanislammari <yanis.lammari937@gmail.com>
2025-01-25 22:34:09 +0100

    add user validation

---
+++ src/domain/validations/user.validation.ts
@@ -0,0 +1,9 @@
+import joi from "joi";
+
+const UserValidation = joi.object({
+  username: joi.string().max(25).required(),
+  email: joi.string().email().required(),
+  password: joi.string().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/).required()
+});
+
+export default UserValidation;
