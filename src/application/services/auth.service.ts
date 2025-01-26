5b1a2ab68eb6ee56567814a72e4e23d61e432f06
yanislammari <yanis.lammari937@gmail.com>
2025-01-26 01:41:55 +0100

    add register service

--- src/application/services/auth.service.ts
+++ src/application/services/auth.service.ts
@@ -1,12 +1,14 @@
+import User from "../../domain/entities/user.entity";
+import UserRepository from "../../infrastructure/repositories/user.repository";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import UserRepository from "../../infrastructure/repositories/user.repository";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET_KEY as string;
 const JWT_EXPIRATION = "1h";
+const SALT_ROUNDS = 10;
 
 export async function loginService(email: string, password: string): Promise<string> {
   try {
@@ -28,5 +30,31 @@ export async function loginService(email: string, password: string): Promise<str
       throw err;
     }
     throw new Error("Error during login");
+  }
+}
+
+export async function registerService(user: User): Promise<string> {
+  try {
+    const existingUser = await UserRepository.getByEmail(user.email);
+    if(existingUser) {
+      throw new Error("Email already exist");
+    }
+
+    const hashedPassword = await bcrypt.hash(user.password, SALT_ROUNDS);
+
+    const newUser = await UserRepository.addUser({
+      username: user.username,
+      email: user.email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign({ id: newUser.id, email: newUser.email }, JWT_SECRET, { expiresIn: JWT_EXPIRATION });
+    return token;
+  }
+  catch(err: any) {
+    if(err.message === "Email already exist") {
+      throw err;
+    }
+    throw new Error("Error during registration");
   }
 }
