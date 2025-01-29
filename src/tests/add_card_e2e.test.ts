be0ed64a16e69aa39d629adbb74c22406fd8bb80
Alexis DUPLESSIS <alexis.duplessis@softathome.com>
2025-01-29 16:21:43 +0100

    Isolation of unit test

--- src/tests/add_card_e2e.test.ts
+++ src/tests/add_card_e2e.test.ts
@@ -1,5 +1,5 @@
 describe('test add of card as a user',()=>{
-    it('should add a card to a user',()=>{
+    it('should add a card as a login user',()=>{
         expect(true).toBe(true);
     });
 });
